71fc4a65dd608d584bc1bf537fc801561984b8ba
Jonatan Dahl <jd@spotify.com>
2014-11-30 11:03:27 +0000

    style tweaks

--- client/client.js
+++ client/client.js
@@ -1,32 +1,38 @@
-if (Meteor.isClient) {
-  var clicked = false;
+(function() {
+
+  // this is prooobably not the meteor way.... !
+
+  var showVerticalSelect = false;
+  Template.body.helpers({
+    showVerticalSelect: function() {
+      return showVerticalSelect.toString();
+    }
+  });
+
   Template.body.events({
     'click .home-query strong': function(event, template) {
       var verticalSelect = template.find('.vertical-select');
       var main = template.find('.main');
-      if (!clicked) {
+      if (!showVerticalSelect) {
         main.className += ' blur';
         verticalSelect.className += ' show';
       } else {
         main.className = main.className.replace(' blur', '');
         verticalSelect.className = verticalSelect.className.replace(' show', '');
       }
-      clicked = !clicked;
-    }
-  })
-
-  Template.body.events({
+      showVerticalSelect = !showVerticalSelect;
+    },
     'click .vertical-select li': function(event, template) {
       var main = template.find('.main');
       var verticalSelect = template.find('.vertical-select');
-      if (!clicked) {
+      if (!showVerticalSelect) {
         verticalSelect.className += ' show';
         main.className += ' blur';
       } else {
         verticalSelect.className = verticalSelect.className.replace(' show', '');
         main.className = main.className.replace(' blur', '');
       }
-      clicked = !clicked;
+      showVerticalSelect = !showVerticalSelect;
     }
   })
-}
+})();
